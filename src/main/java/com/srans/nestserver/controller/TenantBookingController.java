aa2e40003a9d290070f087d6fe8975857952b55f
manishk219009 <42214844+manishk219009@users.noreply.github.com>
2019-12-12 22:26:17 +0530

    TenantBookingController.....

--- src/main/java/com/srans/nestserver/controller/TenantBookingController.java
+++ src/main/java/com/srans/nestserver/controller/TenantBookingController.java
@@ -1,17 +1,24 @@
 package com.srans.nestserver.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.srans.nestserver.exception.ResourceNotFoundException;
 import com.srans.nestserver.model.Floor;
 import com.srans.nestserver.model.Hostel;
 import com.srans.nestserver.model.Room;
@@ -25,89 +32,43 @@ import com.srans.nestserver.repository.TenantRepository;
 @RestController
 @RequestMapping("/api/v1")
 public class TenantBookingController {
-	
-	
+
 	Logger logger = LoggerFactory.getLogger(TenantController.class);
 
 	@Autowired
 	private TenantRepository tenantRepository;
-	
-	
+
 	@Autowired
 	private HostelRepository hostelRepository;
-	
+
 	@Autowired
 	private RoomRepository roomRepository;
-	
+
 	@Autowired
 	private FloorRepository floorRepository;
-	
+
 	@Autowired
 	private BedRepository bedRepository;
-	
-	
+
 	@GetMapping("tenantbooking/hostelName")
 	public List<String> findAll() {
 		return tenantRepository.getAllHostelName();
 
 	}
-	
+
 	@GetMapping("tenantbooking/hostelName/{hostelname}/bed")
-    public List<Long> getNumOfFloor(@PathVariable(value="hostelname") String hostelname) {
-		
-		Integer totalnumberoffloor=tenantRepository.getTotalNumberOfFloors(hostelname);
-		System.out.println(totalnumberoffloor);
-		
-		String floorName=null;
-		Long hostelid=tenantRepository.getHostelId(hostelname);
-		
-		for(int i=1;i<=totalnumberoffloor;i++) {
-			
-				  floorName=i+"st"+"-floor";
-				  if(i==2) {
-					  floorName=i+"nd"+"-floor";
-				  }
-				  
-				  else if(i==3) {
-					  floorName=i+"rd"+"-floor";
-				  }
-				  
-				  else if(i>=4) {
-					  floorName=i+"th"+"-floor";
-				  }
-				 
-				 System.out.println(floorName);
-				 
-				 
-			 
-		}
-		
-		/*
-		 * Hostel hostel=null; Hostel responseHostel = hostelRepository.save(hostel);
-		 * 
-		 * responseHostel.getfloors().forEach(floor -> {
-		 * floor.setHostelId(responseHostel.getId()); Floor resFloor =
-		 * floorRepository.save(floor);
-		 * 
-		 * floor.getRooms().forEach(room -> { room.setHostelId(responseHostel.getId());
-		 * room.setFloorId(resFloor.getId()); Room resRoom = roomRepository.save(room);
-		 * room.getBeds().forEach(bed -> { bed.setHostelId(responseHostel.getId());
-		 * bed.setFloorId(resFloor.getId()); bed.setRoomId(resRoom.getId());
-		 * bedRepository.save(bed); });
-		 * 
-		 * });
-		 * 
-		 * });
-		 * 
-		 */
-		
-		
-		
-		List<Long> l=new ArrayList<>();
-		return l;
-		
+	public List<Object[]> getNumOfFloor(@PathVariable(value = "hostelname") String hostelname) {
 	
+	
+		Long HostelId=tenantRepository.getHostelId(hostelname);
+		
+		System.out.println(HostelId);
+		
+		
+
+		return tenantRepository.getBedInfo(HostelId);
+
 	}
-	
+   
 
 }
--- src/main/java/com/srans/nestserver/repository/TenantRepository.java
+++ src/main/java/com/srans/nestserver/repository/TenantRepository.java
@@ -16,11 +16,19 @@ public interface TenantRepository extends JpaRepository<Tenant, Long>{
 	@Query(value="SELECT hostel_name FROM Hostel", nativeQuery=true) 
 	   public List<String> getAllHostelName();
 	
-	@Query(value="SELECT num_of_floors,id FROM hostel WHERE hostel_name=?1", nativeQuery=true)
-	  public Integer  getTotalNumberOfFloors(String hostelname);
 	
-	@Query(value="SELECT id FROM hostel WHERE hostel_name=?1", nativeQuery=true)
+	
+	@Query(value="SELECT id FROM HOSTEL WHERE hostel_name=?1", nativeQuery=true)
 	  public Long  getHostelId(String hostelname);
+	
+	@Query(value="select * from "
+			+ "hostel h,floor f, room r where "
+			+ "h.id = f.hostel_id "
+			+ "and f.id = r.floor_id "
+			+ "and h.id = r.hostel_id "
+			+ "and h.id=?1"  
+			, nativeQuery=true)
+	 public List<Object[]> getBedInfo(Long hostelId);
 	
 	// public List<Object[]>  getTotalNumberOfFloors(String hostelname);
 
