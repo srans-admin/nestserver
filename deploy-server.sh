762de1c56c622c7d817c1cddc26a38e327d32127
srans-admin <ram.t@srans.in>
2019-12-28 17:04:25 +0530

    deployserver script for devops

---
+++ deploy-server.sh
@@ -0,0 +1,7 @@
+##--no-cache 
+git checkout -f sprint6
+git pull 
+docker build -t nest-server .
+docker stop nest-server-container
+docker rm nest-server-container
+docker run -p 8080:8080 -d -it  --name nest-server-container nest-server
