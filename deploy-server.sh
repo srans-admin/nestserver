f3fe005806e0667d087666ad079a6c8ae28046ed
archana_504 <archana@srans.in>
2019-12-29 10:43:48 +0000

    To deploy on AWS deploy-server.sh

--- deploy-server.sh
+++ deploy-server.sh
@@ -3,10 +3,15 @@
 ## DevOps: Krishna
 
 ##--no-cache 
-
+cd /home/ec2-user/srans/nest-server
 git checkout -f $1
 git pull 
-docker build -t nest-server .
+docker build -f /home/ec2-user/srans/nest-server/Dockerfile -t nest-server .
 docker stop nest-server-container
 docker rm nest-server-container
 docker run -p 8080:8080 -d -it -v /home/ec2-user/srans/nest-server/uploads:/app/uploads/ --name nest-server-container nest-server
+
+## Validation to verify whether docker container loaded successfully
+docker ps -a
+
+docker logs nest-server-container --tail 100
